31945ffbb23bdeeb7b4825d08a21e923c7f556eb
omarShafeeq <omaromar201145@gmail.com>
2026-04-21 22:36:03 +0200

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"dd93de6fb1776398bf586cbd477deade1391c7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "680328860"
+    serviceWorkerVersion: "1980542935"
   }
 });
